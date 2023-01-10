40d52e1aa6dcba916dbf453b38520935df092855
9909Y-Robotics <24battistar@s.mpsct.org>
2023-01-10 11:54:27 -0500

    Initial commit & project setup

---
+++ include/subsystemHeaders/globals.hpp
@@ -0,0 +1,22 @@
+#include "main.h"
+
+// Controllers
+extern pros::Controller controller1;
+extern pros::Controller controller2;
+
+// Motors
+extern pros::Motor driveLeftFront;
+extern pros::Motor driveLeftCenter;
+extern pros::Motor driveLeftBack;
+extern pros::Motor driveRightFront;
+extern pros::Motor driveRightCenter;
+extern pros::Motor driveRightBack;
+extern pros::Motor flywheel;
+extern pros::Motor intake;
+
+// Motor Groups
+extern pros::Motor_Group driveLeft;
+extern pros::Motor_Group driveRight;
+
+// ADI Digital Outs
+extern pros::ADIDigitalOut stringLauncher;
---
+++ src/subsystemFiles/globals.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+
+// Controllers
+pros::Controller controller1(pros::E_CONTROLLER_MASTER);
+pros::Controller controller2(pros::E_CONTROLLER_PARTNER);
+
+// Motors
+pros::Motor driveLeftFront(5, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveLeftCenter(6, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveLeftBack(7, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRightFront(18, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRightCenter(19, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRightBack(20, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor flywheel(11, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake(10, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
+
+// Motor Groups
+pros::Motor_Group driveLeft({driveLeftBack, driveLeftCenter, driveLeftFront});
+pros::Motor_Group driveRight({driveRightBack, driveRightCenter, driveRightFront});
+
+// ADI Digital Outs
+pros::ADIDigitalOut stringLauncher('G', false);
