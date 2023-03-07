01424d61829170fd1c05fe63e5d1572ebd12c5cf
9909Y-Robotics <24battistar@s.mpsct.org>
2023-03-06 22:51:13 -0500

    Perfect rotate() & and update skills auton

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -62,8 +62,6 @@ void autonomous() {
         // Assumes robot is 13" wide and 17.5" long
         // Tare intake motor starting position
         pros::lcd::initialize();
-        pros::lcd::set_text(1, std::to_string(imu.get_rotation()));
-        pros::delay(3000);
         intakeController->tarePosition();
 
         // Drive backwards 2" and spin roller
@@ -76,13 +74,11 @@ void autonomous() {
         // Drive to next roller, get disc, and spin
         chassis->moveDistance(5_in); // 7.75" to clear roller
         rotate(-45, 38);
-        pros::lcd::set_text(1, std::to_string(imu.get_rotation()));
         intake.move(-127);
         chassis->moveDistance(33.94112549695428_in);
         chassis->waitUntilSettled();
         intake.brake();
-        rotate(135, 38);
-        pros::lcd::set_text(2, std::to_string(imu.get_rotation()));
+        rotate(90, 38);
         chassis->moveDistance(-4.5_in); // adjusted for roller 0 -> 1.5
         chassis->waitUntilSettled();
         intake.move(-1200);
@@ -92,8 +88,8 @@ void autonomous() {
 
         // Drive to goal and shoot 3 discs
         chassis->moveDistance(3_in);
+        rotate(0, 38);
         pros::lcd::set_text(3, std::to_string(imu.get_rotation()));
-        rotate(-90, 38);
         chassis->moveDistance(51.25_in);
         flywheel.move(127.0 / 12.0 * 9.3); // 9.3v scaled out of 127
         pros::delay(3000);
@@ -116,9 +112,9 @@ void autonomous() {
         chassis->moveDistance(67.88225099390856_in);
         chassis->waitUntilSettled();
         intake.brake();
-        rotate(-45, 38);
+        rotate(0, 38);
         chassis->moveDistance(48_in);
-        rotate(-45, 38);
+        rotate(-90, 38);
         chassis->moveDistance(3.25_in);
         flywheel.move(127.0 / 12.0 * 9.3); // 9.3v scaled out of 127
         pros::delay(3000);
@@ -137,11 +133,11 @@ void autonomous() {
         // Intake 3 more discs and shoot
         chassis->moveDistance(-3.25_in);
         intake.move(127);
-        rotate(-135, 38);
+        rotate(-225, 38);
         chassis->moveDistance(101.8233764908628_in);
         chassis->waitUntilSettled();
         intake.brake();
-        rotate(45, 38);
+        rotate(-180, 38);
         chassis->moveDistance(3.25_in);
         flywheel.move(127.0 / 12.0 * 9.3); // 9.3v scaled out of 127
         pros::delay(3000);
@@ -161,22 +157,22 @@ void autonomous() {
 
         // Intake 3 more discs, spin 2 rollers, fire 3 discs, shoot string from corner
         chassis->moveDistance(-3.25_in);
-        rotate(135, 38);
+        rotate(-225, 38);
         chassis->moveDistance(-2.121320343559643_in);
         intake.move(127);
-        rotate(135, 38);
+        rotate(-315, 38);
         chassis->moveDistance(2.121320343559643_in);
         chassis->waitUntilSettled();
         intake.brake();
         chassis->moveDistance(-1.414213562373095_in);
-        rotate(-135, 38);
+        rotate(90, 38);
         chassis->moveDistance(-25.75_in); // adjust for roller 0 -> 1.5
         chassis->waitUntilSettled();
         intakeController->setTarget(-1200);
         intakeController->waitUntilSettled();
         intakeController->tarePosition();
         chassis->moveDistance(25.75_in);
-        rotate(-90, 38);
+        rotate(0, 38);
         chassis->moveDistance(-25.75_in); // adjust for roller 126.5 -> 125
         chassis->waitUntilSettled();
         intakeController->setTarget(-1200);
@@ -199,7 +195,7 @@ void autonomous() {
         pros::delay(1000);
         flywheel.brake();
         chassis->moveDistance(-75.25_in);
-        rotate(-45, 38);
+        rotate(-135, 38);
         stringLauncher1.set_value(1);
         stringLauncher2.set_value(1);
     }
--- src/subsystemFiles/drive.cpp
+++ src/subsystemFiles/drive.cpp
@@ -49,32 +49,58 @@ void translate(int units, int voltage) {
 
 void rotate(int degrees, int voltage) {
     // Define a direction based on the units provided
-    int direction = abs(degrees) / degrees;
-    // Reset gyro rotation
-    imu.tare_rotation();
+    int direction = 1;
+    if ( degrees < imu.get_rotation() ) {
+        direction = -1;
+    }
     // Turn until units - 5 degrees are reached
     setDrive(voltage * direction, -voltage * direction);
-    while ( fabs(imu.get_rotation()) < abs(degrees) - 5 ) {
-        pros::delay(10);
+    if ( direction == 1 ) {
+        while ( imu.get_rotation() < degrees - 5 ) {
+            pros::delay(10);
+        }
+    }
+    else {
+        while ( imu.get_rotation() > degrees + 5 ) {
+            pros::delay(10);
+        }
     }
     // Wait for robot to loose momentum before performing corrections
     setDrive(0, 0);
     pros::delay(100);
-    // Correct for overshoot
-    if ( fabs(imu.get_rotation()) > abs(degrees) ) {
-        setDrive(-38 * direction, 38 * direction);
-        while ( fabs(imu.get_rotation()) > abs(degrees) ) {
-            pros::delay(10);
+    if ( direction == 1 ) {
+        // Correct for overshoot
+        if ( imu.get_rotation() > degrees ) {
+            setDrive(-38 * direction, 38 * direction);
+            while ( imu.get_rotation() > degrees ) {
+                pros::delay(10);
+            }
+        }
+        // Correct for undershoot
+        else if ( imu.get_rotation() < degrees ) {
+            setDrive(38 * direction, -38 * direction);
+            while ( imu.get_rotation() < degrees ) {
+                pros::delay(10);
+            }
         }
     }
-    // Correct for undershoot
-    else if ( fabs(imu.get_rotation()) < abs(degrees) ) {
-        setDrive(38 * direction, -38 * direction);
-        while ( fabs(imu.get_rotation()) < abs(degrees) ) {
-            pros::delay(10);
+    else {
+        // Correct for overshoot
+        if ( imu.get_rotation() > degrees ) {
+            setDrive(-38 * direction, 38 * direction);
+            while ( imu.get_rotation() < degrees ) {
+                pros::delay(10);
+            }
+        }
+        // Correct for undershoot
+        else if ( imu.get_rotation() < degrees ) {
+            setDrive(38 * direction, -38 * direction);
+            while ( imu.get_rotation() > degrees ) {
+                pros::delay(10);
+            }
         }
     }
     // Reset drive to zero
-       setDrive(0, 0);
-       resetDriveEncoders();
+    setDrive(0, 0);
+    resetDriveEncoders();
 }
