2a7454194fac286cbb80451a8f11e06034575013
9909Y-Robotics <24battistar@s.mpsct.org>
2023-04-18 12:00:32 -0400

    Update drivetrain motor ports

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -10,9 +10,9 @@ okapi::Controller controller2(ControllerId::partner);
 
 // Motors
 // Each raw encoder count is 0.375 degrees, so there are 960 in one revolution
-pros::Motor driveLeftFront(5, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor driveLeftCenter(10, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS); // moved from port 6
-pros::Motor driveLeftBack(7, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveLeftFront(2, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveLeftCenter(3, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS); // moved from port 6
+pros::Motor driveLeftBack(4, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor driveRightFront(16, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor driveRightCenter(17, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor driveRightBack(20, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
@@ -29,12 +29,12 @@ pros::ADIDigitalOut stringLauncher2('B', false);
 pros::ADIDigitalOut blooper('H', false);
 
 // Inertial Sensor
-pros::Imu imu(4);
+pros::Imu imu(1);
 
 // Chassis Controller PID
 std::shared_ptr<okapi::ChassisController> chassis =
     ChassisControllerBuilder()
-        .withMotors({5, 6, 7}, {16, 17, 20})
+        .withMotors({2, 3, 4}, {16, 17, 20})
         // Speed gearset, 36:48 gear ratio, 3.25" wheel diameter, 10.75" wheel track0
         //.withDimensions({AbstractMotor::gearset::blue, (48.0 / 36.0)}, {{3.25_in, 25_in}, imev5BlueTPR})
         .withDimensions({AbstractMotor::gearset::blue, (48.0 / 36.0)}, {{3.25_in, 10.75_in}, imev5BlueTPR})
@@ -48,7 +48,7 @@ std::shared_ptr<okapi::ChassisController> chassis =
 // Chassis Controller NO PID
 std::shared_ptr<okapi::ChassisController> NOPIDchassis =
     ChassisControllerBuilder()
-      .withMotors({5, 6, 7}, {16, 17, 20})
+      .withMotors({2, 3, 4}, {16, 17, 20})
       // Speed gearset, 36:48 gear ratio, 3.25" wheel diameter, 10.75" wheel track0
       //.withDimensions({AbstractMotor::gearset::blue, (48.0 / 36.0)}, {{3.25_in, 25_in}, imev5BlueTPR})
       .withDimensions({AbstractMotor::gearset::blue, (48.0 / 36.0)}, {{3.25_in, 10.75_in}, imev5BlueTPR})
